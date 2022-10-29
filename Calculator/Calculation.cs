edab4ee4ff1816013fa92a3190776fe844f2ac9b
Anh Duy <2051012014duy@ou.edu.vn>
2022-10-29 12:58:02 +0700

    BTH_02

---
+++ Calculator/Calculation.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    public class Calculation // Nhớ public nó ra mới xài được không là giữ 1 mình xài luôn
+    {
+        private int a, b;
+        public Calculation(int a , int b)
+        {
+            this.a = a;
+            this.b = b;
+        }
+        public int Exectute(string operation)
+        {
+            int result = 0;
+            switch(operation)
+            {
+                case "+":
+                    result = a + b;
+                    break;
+                case "-":
+                    result = a - b;
+                    break;
+                case "*":
+                    result = a * b;
+                    break;
+                case "/":
+                    result = a / b;
+                    break;
+            }
+            return result;
+        }
+    }
+}
---
+++ CalculatorTester/UnitTest1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Calculator;
+namespace CalculatorTester
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        private Calculation c;
+        [TestInitialize]
+        public void SetUp()
+        {
+            this.c = new Calculation(10, 5);
+        }
+        [TestMethod] // 1 test case
+        public void TestAddOperator()
+        {
+            int expected, actual;
+            //Calculation c = new Calculation(10, 5);
+            expected = 15;
+            actual = c.Exectute("+");
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void TestSubOperator()
+        {
+            int expected, actual;
+            //Calculation c = new Calculation(10, 5);
+            expected = 5;
+            actual = c.Exectute("-");
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void TestMulOperator()
+        {
+            int expected, actual;
+            //Calculation c = new Calculation(10, 5);
+            expected = 50;
+            actual = c.Exectute("*");
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void TestDivOperator()
+        {
+            int expected, actual;
+            //Calculation c = new Calculation(10, 5);
+            expected = 2;
+            actual = c.Exectute("/");
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        [ExpectedException(typeof(DivideByZeroException))]
+        public void TestDivbyZero()
+        {
+            int expected, actual;
+            Calculation c = new Calculation(10, 0);
+            expected = 2;
+            actual = c.Exectute("/");
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
